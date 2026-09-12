097a97f0cd2dea5ada3c782026b50b0d322c7dee
romansouffir-png <roman.souffir@gmail.com>
2026-09-12 15:38:54 +0200

    Update _recipes_custom.py

--- _recipes_custom.py
+++ _recipes_custom.py
@@ -1,18 +1,19 @@
 from typing import List
 
-from _recipe_utils import Recipe
+from _recipe_utils import CoverOptions, Recipe
 
-# Define the categories display order, optional
-categories_sort: List[str] = []
-
-# Define your custom recipes list here
-# Example: https://github.com/ping/newsrack-fork-test/blob/custom/_recipes_custom.py
+categories_sort: List[str] = ["News"]
 
 recipes: List[Recipe] = [
-    # Recipe(
-    #     recipe="example",
-    #     slug="example",
-    #     src_ext="epub",
-    #     category="example",
-    # ),
+    Recipe(
+        recipe="ft",
+        slug="ft-online",
+        src_ext="mobi",
+        target_ext=["epub"],
+        category="News",
+        tags=["business"],
+        cover_options=CoverOptions(
+            logo_path_or_url="https://www.ft.com/partnercontent/content-hub/static/media/ft-horiz-new-black.215c1169.png"
+        ),
+    ),
 ]
